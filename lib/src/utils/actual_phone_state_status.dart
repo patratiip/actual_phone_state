fdcb1742e990c215400bf4fba18094e93cf5ce4d
patratiip <154238580+patratiip@users.noreply.github.com>
2026-03-09 10:23:40 +0100

    docs: add DartDoc comments and test Android foreground/background behavior

--- lib/src/utils/actual_phone_state_status.dart
+++ lib/src/utils/actual_phone_state_status.dart
@@ -1,5 +1,16 @@
+/// Represents the current phone call state on the device.
+///
+/// This enum is returned by [ActualPhoneState.getPhoneState].
 enum ActualPhoneStateStatus {
+  /// The phone state could not be determined.
+  ///
+  /// This may happen if the required permissions are not granted
+  /// or if the platform restricts access to the call state.
   unknown,
+
+  /// No phone call is currently active.
   noCall,
+
+  /// There is an active or incoming phone call.
   callActive,
 }
